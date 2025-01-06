7293d48351de161040c33779075b07e543610b33
Abisheknewton <notwendextres@gmail.com>
2025-01-06 15:57:05 +0530

    backend folder creadted

---
+++ rides API/rides-API/src/main/java/com/newton/rides/RidesApiApplication.java
@@ -0,0 +1,13 @@
+package com.newton.rides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidesApiApplication.class, args);
+	}
+
+}
---
+++ rides API/rides-API/src/test/java/com/newton/rides/RidesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newton.rides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
