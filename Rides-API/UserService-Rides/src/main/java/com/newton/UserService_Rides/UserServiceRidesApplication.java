6af304fc0543e249a1584aeb07062de979c8d4fd
Abishek Newton <abisheknewton.22cs@licet.ac.in>
2025-04-21 16:29:46 +0530

    initial commit of user service

---
+++ Rides-API/UserService-Rides/src/main/java/com/newton/UserService_Rides/UserServiceRidesApplication.java
@@ -0,0 +1,13 @@
+package com.newton.UserService_Rides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceRidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceRidesApplication.class, args);
+	}
+
+}
---
+++ Rides-API/UserService-Rides/src/test/java/com/newton/UserService_Rides/UserServiceRidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newton.UserService_Rides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceRidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
